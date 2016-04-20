4ed2b32aa9f00dca3aeeb0ff5448850a754dac8a
sunrain990 <409747494@qq.com>
2016-04-20 14:46:51 +0800

    add fakuser to sockets

--- sockets.js
+++ sockets.js
@@ -212,87 +212,88 @@ module.exports =function(io,redis,my,mysql_pool,moment,reward,http,log4js){
     //    }
     //});
 
-    //mysql.chat.pool.getConnection(function(err, connection) {
-    //    if(!err){
-    //        connection.query(injectUserSql, function (err, ret) {
-    //            if(!err){
-    //
-    //                var resed = _.sampleSize(ret, _.random(100,150));
-    //
-    //                var rrr = _.map(resed,function(i){
-    //                    return i.id;
-    //                });
-    //
-    //                async.each(rrr,function(item,callback){
-    //                    var data = {
-    //                        data_uid:item
-    //                    };
-    //                    //console.log('servicelist',item);
-    //
-    //
-    //                    data = qs.stringify(data);
-    //                    var opt = {
-    //                        method: "POST",
-    //                        host: "www.geminno.cn",
-    //                        port: 80,
-    //                        path: "/project/index.php/api/user/getinfo",
-    //                        headers: {
-    //                            "Content-Type": 'application/x-www-form-urlencoded',
-    //                            "Content-Length": data.length
-    //                        }
-    //                    };
-    //
-    //                    var req = http.request(opt, function (serverFeedback) {
-    //                        if (serverFeedback.statusCode == 200) {
-    //                            var body = "";
-    //                            serverFeedback.on('data', function (data) { body += data; })
-    //                                .on('end', function () {
-    //                                    var body1 = (new Function("","return "+body))();
-    //                                    console.log(body1);
-    //
-    //                                    if(body1.code == 1){
-    //                                        var fakuser = {
-    //                                            userid:body1.data.id,
-    //                                            //nickname:'今天我值班',
-    //                                            nickname:body1.data.name,
-    //                                            thum:body1.data.thum,
-    //                                            isofficer:body1.data.isofficer
-    //                                        };
-    //                                        console.log(fakuser);
-    //                                        fakusers.push(fakuser);
-    //                                    }else{
-    //                                        console.log('no id-1');
-    //                                    }
-    //
-    //                                });
-    //                        }
-    //                        else {
-    //                            console.log('else');
-    //                        }
-    //                    });
-    //                    req.write(data + "\n");
-    //                    req.on('err',function(e){
-    //                        console.log(e);
-    //                    })
-    //                    req.end();
-    //                },function(err){
-    //                    if(!err){
-    //                        connection.release();
-    //                        console.log(fakusers,'> done2');
-    //                    }else{
-    //                        console.log('> done1');
-    //                    }
-    //                });
-    //            }else{
-    //                console.log(err);
-    //            }
-    //        });
-    //    }else{
-    //        console.log(err);
-    //    }
-    //    // Use the connection
-    //
-    //});
+    //虚拟用户
+    mysql_pool.chat.pool.getConnection(function(err, connection) {
+        if(!err){
+            connection.query(injectUserSql, function (err, ret) {
+                if(!err){
+
+                    var resed = _.sampleSize(ret, _.random(100,150));
+
+                    var rrr = _.map(resed,function(i){
+                        return i.id;
+                    });
+
+                    async.each(rrr,function(item,callback){
+                        var data = {
+                            data_uid:item
+                        };
+                        //console.log('servicelist',item);
+
+
+                        data = qs.stringify(data);
+                        var opt = {
+                            method: "POST",
+                            host: "www.geminno.cn",
+                            port: 80,
+                            path: "/project/index.php/api/user/getinfo",
+                            headers: {
+                                "Content-Type": 'application/x-www-form-urlencoded',
+                                "Content-Length": data.length
+                            }
+                        };
+
+                        var req = http.request(opt, function (serverFeedback) {
+                            if (serverFeedback.statusCode == 200) {
+                                var body = "";
+                                serverFeedback.on('data', function (data) { body += data; })
+                                    .on('end', function () {
+                                        var body1 = (new Function("","return "+body))();
+                                        console.log(body1);
+
+                                        if(body1.code == 1){
+                                            var fakuser = {
+                                                userid:body1.data.id,
+                                                //nickname:'今天我值班',
+                                                nickname:body1.data.name,
+                                                thum:body1.data.thum,
+                                                isofficer:body1.data.isofficer
+                                            };
+                                            console.log(fakuser);
+                                            fakusers.push(fakuser);
+                                        }else{
+                                            console.log('no id-1');
+                                        }
+
+                                    });
+                            }
+                            else {
+                                console.log('else');
+                            }
+                        });
+                        req.write(data + "\n");
+                        req.on('err',function(e){
+                            console.log(e);
+                        })
+                        req.end();
+                    },function(err){
+                        if(!err){
+                            connection.release();
+                            console.log(fakusers,'> done2');
+                        }else{
+                            console.log('> done1');
+                        }
+                    });
+                }else{
+                    console.log(err);
+                }
+            });
+        }else{
+            console.log(err);
+        }
+        // Use the connection
+
+    });
 
 
     //redis.pub.keys('*',function(err,res){
