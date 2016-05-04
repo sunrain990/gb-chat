a7384f8e25b3851fc33101571d24e157eea54549
sunrain990 <409747494@qq.com>
2016-05-04 17:24:09 +0800

    modify array async

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var moment =require('moment');
 var Redis = require('../config/red-is');
 var Memoto = require('../config/memoto/memoto');
+var async = require('async');
 
 /* GET home page. */
 router.get('/test', function(req, res, next) {
@@ -132,41 +133,126 @@ router.post('/chat/notify1',function(req,res,next){
     }});
 });
 
+//router.post('/chat/notify',function(req,res,next){
+//    var badge = req.body.badge;
+//    var share = req.body.share;
+//    var answer = req.body.answer;
+//    var gold = req.body.gold;
+//    var time = req.body.time;
+//    var userid = req.body.userid;
+//    var data = req.body.data;
+//    var code = req.body.code;
+//
+//    console.log(time,userid ,'this is u' + 'serid and time---------------------<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>---------------------');
+//
+//    var msg = {
+//        badge:badge,
+//        userid:userid,
+//        time:time,
+//        share:share,
+//        gold:gold,
+//        data:data,
+//        code:code
+//    };
+//    Redis.store.smembers("userid"+msg.userid,function(err,res){
+//        if(err){
+//            console.log('私信错误：'+err);
+//        }
+//        else{
+//            res.forEach(function(i){
+//                console.log('heheda',i);
+//                //Memoto.io.to(i).emit('badge',msg);
+//                Memoto.io.in(i).emit('notify',msg);
+//            });
+//        }
+//    });
+//    res.json({code:1,text:'success',data:{}});
+//});
+
 router.post('/chat/notify',function(req,res,next){
-    var badge = req.body.badge;
-    var share = req.body.share;
-    var answer = req.body.answer;
-    var gold = req.body.gold;
-    var time = req.body.time;
-    var userid = req.body.userid;
     var data = req.body.data;
-    var code = req.body.code;
+    console.log(req.body,'this is notify data!');
+    console.log(req.body.data.userid,'this is notify data1!');
 
-    console.log(time,userid ,'this is u' + 'serid and time---------------------<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>---------------------');
 
-    var msg = {
-        badge:badge,
-        userid:userid,
-        time:time,
-        share:share,
-        gold:gold,
-        data:data,
-        code:code
-    };
-    Redis.store.smembers("userid"+msg.userid,function(err,res){
-        if(err){
-            console.log('私信错误：'+err);
-        }
-        else{
-            res.forEach(function(i){
-                console.log('heheda',i);
-                //Memoto.io.to(i).emit('badge',msg);
-                Memoto.io.in(i).emit('notify',msg);
-            });
+    async.each(data,function(item,callback){
+        var userid = item.userid;
+
+
+        Redis.store.smembers("userid"+userid,function(err,res){
+            if(err){
+                console.log('私信错误：'+err);
+            }
+            else{
+                res.forEach(function(i){
+                    console.log('heheda',i);
+                    //Memoto.io.to(i).emit('badge',msg);
+                    Memoto.io.in(i).emit('notify',item);
+                });
+            }
+        });
+
+    },function(err){
+        if(!err){
+            console.log('noerro > done2');
+        }else{
+            console.log('> done1');
         }
     });
+
+    //var data = [
+    //    {
+    //        "userid": "1",
+    //        "type": "notice",
+    //        "url": "/_pages/contact/notice.html",
+    //        "content": "您提出的问题[C++初始化]有了新回复点击<a href='/_pages/faq/faqDetails.html?faqid=11957'>这里</a>"
+    //    },
+    //    {
+    //        "userid": "2",
+    //        "type": "notice",
+    //        "url": "/_pages/contact/notice.html",
+    //        "content": "您提出的问题[C++初始化]有了新回复点击<a href='/_pages/faq/faqDetails.html?faqid=11957'>这里</a>"
+    //    },
+    //    {
+    //        "userid": "1",
+    //        "type": "notice",
+    //        "url": "/_pages/contact/notice.html",
+    //        "content": "您提出的问题[C++初始化]有了新回复点击<a href='/_pages/faq/faqDetails.html?faqid=11957'>这里</a>"
+    //    }
+    //];
+
+    //var share = req.body.share;
+    //var answer = req.body.answer;
+    //var gold = req.body.gold;
+    //var time = req.body.time;
+    //var userid = req.body.userid;
+    //var data = req.body.data;
+    //var code = req.body.code;
+    //
+    //console.log(time,userid ,'this is u' + 'serid and time---------------------<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>---------------------');
+    //
+    //var msg = {
+    //    badge:badge,
+    //    userid:userid,
+    //    time:time,
+    //    share:share,
+    //    gold:gold,
+    //    data:data,
+    //    code:code
+    //};
+    //Redis.store.smembers("userid"+msg.userid,function(err,res){
+    //    if(err){
+    //        console.log('私信错误：'+err);
+    //    }
+    //    else{
+    //        res.forEach(function(i){
+    //            console.log('heheda',i);
+    //            //Memoto.io.to(i).emit('badge',msg);
+    //            Memoto.io.in(i).emit('notify',msg);
+    //        });
+    //    }
+    //});
     res.json({code:1,text:'success',data:{}});
 });
-
 
 module.exports = router;
